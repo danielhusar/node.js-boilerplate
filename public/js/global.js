0e014e891e295756dc6b70d8c727de89ed327a8e
Daniel Husar <dano.husar@gmail.com>
2013-01-28 09:57:43 +0100

    app changes

--- public/js/global.js
+++ public/js/global.js
@@ -21,12 +21,12 @@
     'desktop'  : function () { return APP.device.ie() || Modernizr.mq('only screen and (min-width: 1025px)'); },
     'tablet'   : function () { return (! APP.device.desktop()) && Modernizr.mq('only screen and (min-width: 569px)'); },
     'mobile'   : function () { return ! (APP.device.tablet() || APP.device.desktop()); },
+		'ieMobile' : function () { return (/IEMobile/i).test(window.navigator.userAgent); },
     'ie'       : function () { return !! ($.browser.msie && ! APP.device.ieMobile()); }
   };
 	
 	/**
    * Function that will execute callback only if one of devices passes
-   *
    * Especially useful for executing some for mobile only, tablets only, IE only etc.
    *
    * Sample Usage:
@@ -34,31 +34,21 @@
    *   // Set up a namespace
    *   namespace('APP');
    *
-   *   APP.for('mobile', callback);                      // => V will run the callback  (just for the mobile)
-	 *   APP.for('desktop', callback);                     // => V will run the callback  (just for the desktop)
-	 *   APP.for('mobile,desktop', callback);              // => V will run the callback  (for the mobile and desktop)
-
+   *   APP.isDevice('mobile', callback);                      // => V will run the callback  (just for the mobile)
+	 *   APP.isDevice('desktop', callback);                     // => V will run the callback  (just for the desktop)
+	 *   APP.isDevice('mobile,desktop', callback);              // => V will run the callback  (for the mobile and desktop)
    *
-   * @namespace APP
-   * @chainable
    *
    * @param     {Anything}  conditions  If conditions is a string, named conditions would be used, otherwise, value would be casted to bool (described above)
    * @param     {Function}  callback    Callback to be triggered when conditions return true
-   * @return    {Object}                APP Object (so you can chain methods)
+   * @return    {Object}                APP Object 
    */
-  APP.for = function (conditions, callback) {
-    var result;
-    var method;
+  APP.isDevice = function (conditions, callback) {
+		conditions = conditions.split(','); // create an array of device
 
-    if (_.isString(conditions))  {
-
-      conditions = conditions.split(','); // create an array of device
-
-      result     = _.some(conditions, function (condition) {
-        return !! (_.result(APP.device, condition) || false);
-      });
-
-    }
+		var result     = _.some(conditions, function (condition) {
+			return !! (_.result(APP.device, condition) || false);
+		});
 
     if (result) {
       callback();
@@ -73,32 +63,18 @@
    *
    * Sample Usage:
    *
-   *
    *   1. Given I am on 'home' page:
    *
-   *     APP.onPages('home,contact', function (page) {
+   *     APP.isPage('home,contact', function (page) {
    *       console.log('Code Executed Successfully on page "' + page + '"');
    *     });
    *     // => Code Executed Successfully on page "home" (Pages allowed: "home,vod")
    *
-   *
-   *   2. Given I am on 'about' page:
-   *
-   *     APP.onPages('home,contact', function (page) {
-   *       console.log('Code Executed Successfully on page "' + page + '"');
-   *     });
-   *     // (nothing happens)
-   *
-   *
-   *
-   *
-   * @chainable
-   *
    * @param  {String}   pageName  Comma separated page names to execute callback on, if 'all' is provided, will always execute
    * @param  {Function} callback  Callback to execute, takes one param (string) - page name
-   * @return {Object}             APP Object, chainable method
+   * @return {Object}             APP Object
    */
-	APP.onPages = function (pageNames, callback) {
+	APP.isPage = function (pageNames, callback) {
     var pages;
     var currentPage;
 
@@ -108,34 +84,40 @@
     currentPage = (window.location.pathname && window.location.pathname != '/') ? window.location.pathname : '/home';
 
     if (_.contains(pages, currentPage)) {
-      callback(APP.settings.environment.page.path);
+      callback();
     }
 
     return APP;
   };
 
+	
+	/*
+	*
+	* @namespace window
+	* Avoid `console` errors in browsers that lack a console.
+	*
+  */	
+	(function() {
+			var method;
+			var noop = function () {};
+			var methods = [
+					'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+					'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+					'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+					'timeStamp', 'trace', 'warn'
+			];
+			var length = methods.length;
+			var console = (window.console = window.console || {});
+
+			while (length--) {
+					method = methods[length];
+
+					// Only stub undefined methods.
+					if (!console[method]) {
+							console[method] = noop;
+					}
+			}
+	}());
+
+
 })(this, this.document);
-
-
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
