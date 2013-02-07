3b749259b080665edcc7f1c118f0fa9d8aabee8a
Daniel Husar <dano.husar@gmail.com>
2013-02-07 17:37:46 +0100

    test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -9,7 +9,7 @@
  * @param  {object} app     app object
  * @param  {object} model   model object that belong to the current controller
  * @param  {object} helpers helpers object
- * @return {void}
+ * @return {void} 
  */
 module.exports = function(app, model, helpers){
 
